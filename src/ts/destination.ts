4d6c9136f209440c779313941d008c1ced0258fd
Marek~ <s.marekszwed@gmail.com>
2024-02-04 22:06:26 +0100

    added if...else statement for e.target

--- src/ts/destination.ts
+++ src/ts/destination.ts
@@ -1,6 +1,6 @@
 import jsonFile from '../files/data.json';
 
-const destinationNav = document.querySelectorAll('.subnav__nav--item');
+const destinationNav: NodeList = document.querySelectorAll('.subnav__nav--item');
 
 let planetImage = document.querySelector('.left-side__planet-img') as HTMLImageElement;
 
@@ -20,45 +20,49 @@ destinationNav.forEach((navItem) => {
 		const menuItem = e.target as HTMLElement;
 		
 
-		switch (menuItem.innerText) {
-			case 'MOON':				
-				planetImage.src = "./src/assets/destination/image-moon.png"							
-				planetName.innerText = jsonFile.destinations[0].name
-				planetDescription.innerText = jsonFile.destinations[0].description
-				avgDistance.innerText = jsonFile.destinations[0].distance;
-				avgTime.innerText = jsonFile.destinations[0].travel
-				
-				break;
-
-			case 'MARS':			
-				planetImage.src = "./src/assets/destination/image-mars.png"					
-				planetName.innerText = jsonFile.destinations[1].name
-				planetDescription.innerText = jsonFile.destinations[1].description
-				avgDistance.innerText = jsonFile.destinations[1].distance;
-				avgTime.innerText = jsonFile.destinations[1].travel
-				
-				break;
-
-			case 'EUROPA':
-				planetImage.src = "./src/assets/destination/image-europa.png"			
-				planetName.innerText = jsonFile.destinations[2].name
-				planetDescription.innerText = jsonFile.destinations[2].description
-				avgDistance.innerText = jsonFile.destinations[2].distance;
-				avgTime.innerText = jsonFile.destinations[2].travel		
-
-				break;
-
-			case 'TITAN':		
-				planetImage.src = "./src/assets/destination/image-titan.png"
-				planetName.innerText = jsonFile.destinations[3].name
-				planetDescription.innerText = jsonFile.destinations[3].description
-				avgDistance.innerText = jsonFile.destinations[3].distance;
-				avgTime.innerText = jsonFile.destinations[3].travel
-				
-				break;
-
-			default:
-				break;
-		}
+		if(menuItem != null) {
+			switch (menuItem.innerText) {
+				case 'MOON':				
+					planetImage.src = "./src/assets/destination/image-moon.png"							
+					planetName.innerText = jsonFile.destinations[0].name
+					planetDescription.innerText = jsonFile.destinations[0].description
+					avgDistance.innerText = jsonFile.destinations[0].distance;
+					avgTime.innerText = jsonFile.destinations[0].travel
+					
+					break;
+	
+				case 'MARS':			
+					planetImage.src = "./src/assets/destination/image-mars.png"					
+					planetName.innerText = jsonFile.destinations[1].name
+					planetDescription.innerText = jsonFile.destinations[1].description
+					avgDistance.innerText = jsonFile.destinations[1].distance;
+					avgTime.innerText = jsonFile.destinations[1].travel
+					
+					break;
+	
+				case 'EUROPA':
+					planetImage.src = "./src/assets/destination/image-europa.png"			
+					planetName.innerText = jsonFile.destinations[2].name
+					planetDescription.innerText = jsonFile.destinations[2].description
+					avgDistance.innerText = jsonFile.destinations[2].distance;
+					avgTime.innerText = jsonFile.destinations[2].travel		
+	
+					break;
+	
+				case 'TITAN':		
+					planetImage.src = "./src/assets/destination/image-titan.png"
+					planetName.innerText = jsonFile.destinations[3].name
+					planetDescription.innerText = jsonFile.destinations[3].description
+					avgDistance.innerText = jsonFile.destinations[3].distance;
+					avgTime.innerText = jsonFile.destinations[3].travel
+					
+					break;
+	
+				default:
+					break;
+			}
+		} else {
+			console.error('e.target (menuItem) is null');		
+		}		
 	});
 });
