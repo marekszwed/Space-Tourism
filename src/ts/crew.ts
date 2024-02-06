d8989e608b5c0df6661a894ade017d03810746b2
Marek~ <s.marekszwed@gmail.com>
2024-02-06 23:08:46 +0100

    Update code

--- src/ts/crew.ts
+++ src/ts/crew.ts
@@ -5,9 +5,12 @@ import jsonFile from '../files/data.json';
 // Btns
 const circleBtns: NodeList = document.querySelectorAll('.memberBtn')
 // personal info
-const crewRole = document.querySelector('.members__left--position') as HTMLElement
-const memberName = document.querySelector('.members__left--name') as HTMLElement
-const description = document.querySelector('.description') as HTMLElement
+const crewRole: HTMLHeadElement = document.querySelector('.members__left--position')!
+const memberName: HTMLHeadElement = document.querySelector('.members__left--name')!
+const description:HTMLParagraphElement = document.querySelector('.description')!
+const memberImg: HTMLImageElement = document.querySelector('.members__right--member-img')!
+
+
 
 
 circleBtns.forEach((item) => item.addEventListener('click', (e) => {
@@ -22,21 +25,25 @@ circleBtns.forEach((item) => item.addEventListener('click', (e) => {
                 crewRole.innerText = jsonFile.crew[0].role
                 memberName.innerText = jsonFile.crew[0].name
                 description.innerText = jsonFile.crew[0].bio
+                memberImg.src = "./src/assets/crew/image-1.png"
                 break;
             case "1":
                 crewRole.innerText = jsonFile.crew[1].role
                 memberName.innerText = jsonFile.crew[1].name
                 description.innerText = jsonFile.crew[1].bio
+                memberImg.src = "./src/assets/crew/image-2.png"
                 break;
             case "2":
                 crewRole.innerText = jsonFile.crew[2].role
                 memberName.innerText = jsonFile.crew[2].name
                 description.innerText = jsonFile.crew[2].bio
+                memberImg.src = "./src/assets/crew/image-3.png"
                 break;
             case "3":
                 crewRole.innerText = jsonFile.crew[3].role
                 memberName.innerText = jsonFile.crew[3].name
                 description.innerText = jsonFile.crew[3].bio
+                memberImg.src = "./src/assets/crew/image-4.png"
                 break;
         
             default:
