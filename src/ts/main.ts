2f159fc4691a3243b234e77fdb86588908ccf999
Marek~ <s.marekszwed@gmail.com>
2024-02-07 22:47:08 +0100

    added function which toggle class between buttons

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,7 @@
-import '../sass/main.scss';
+import '../sass/main.scss'
 import '../ts/destination'
 import '../ts/crew'
+import '../ts/technology'
 
 const navItem = document.querySelectorAll('.nav__item');
 
--- src/ts/technology.ts
+++ src/ts/technology.ts
@@ -1,3 +1,19 @@
 "use strict"
 
+import jsonFile from '../files/data.json';
 
+const circles = document.querySelectorAll('.circle')
+
+
+function openActiveItem (this: HTMLButtonElement) {
+    closeActiveItem()
+    this.classList.toggle('circle-active')
+}
+
+function closeActiveItem () {
+    circles.forEach(btn => btn.classList.remove('circle-active'))
+}
+
+
+
+circles.forEach(button => button.addEventListener('click', openActiveItem))
