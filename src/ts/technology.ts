fce172c45880ff749130ac8d4aa6dc66f522d561
Marek~ <s.marekszwed@gmail.com>
2024-02-10 10:54:23 +0100

    Updated code for 'technology' subpage

--- src/ts/technology.ts
+++ src/ts/technology.ts
@@ -3,6 +3,9 @@
 import jsonFile from '../files/data.json';
 
 const circles = document.querySelectorAll('.circle')
+const title = document.querySelector('.title') as HTMLHeadElement
+const description = document.querySelector('.vehicle-description') as HTMLParagraphElement
+const image = document.querySelector('.main__right--vehicle-img') as HTMLImageElement
 
 
 function openActiveItem (this: HTMLButtonElement) {
@@ -13,6 +16,24 @@ function openActiveItem (this: HTMLButtonElement) {
 function closeActiveItem () {
     circles.forEach(btn => btn.classList.remove('circle-active'))
 }
+
+circles.forEach(btn => btn.addEventListener('click', ((e) => {
+    const circleBtn = e.target as HTMLButtonElement
+    let circleBtnNumber = Number(circleBtn.innerText)
+
+    console.log(circleBtn);
+    console.log(circleBtnNumber);
+
+    if(circleBtn) {
+        title.innerText = jsonFile.technology[circleBtnNumber -1].name
+        description.innerText = jsonFile.technology[circleBtnNumber -1].description
+        image.src = `./src/assets/technology/image-${circleBtnNumber}-landscape.jpg`		
+    }
+    
+
+})))
+
+
 
 
 
