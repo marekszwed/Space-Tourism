80b51fe60fa93ffeeed00cc19d76747ee873284a
Marek~ <s.marekszwed@gmail.com>
2024-02-20 22:56:13 +0100

    Updated code - added function which add and remove class active on buttons

--- src/ts/crew.ts
+++ src/ts/crew.ts
@@ -1,56 +1,72 @@
-"use strict"
+'use strict';
 
 import jsonFile from '../files/data.json';
 
 // Btns
-const circleBtns: NodeList = document.querySelectorAll('.memberBtn')
+const circleBtns = document.querySelectorAll('.memberBtn');
 // personal info
-const crewRole: HTMLHeadElement = document.querySelector('.members__left--position')!
-const memberName: HTMLHeadElement = document.querySelector('.members__left--name')!
-const description:HTMLParagraphElement = document.querySelector('.description')!
-const memberImg: HTMLImageElement = document.querySelector('.members__right-img')!
+const crewRole: HTMLHeadElement = document.querySelector(
+	'.members__left--position'
+)!;
+const memberName: HTMLHeadElement = document.querySelector(
+	'.members__left--name'
+)!;
+const description: HTMLParagraphElement =
+	document.querySelector('.description')!;
+const memberImg: HTMLImageElement = document.querySelector(
+	'.members__right-img'
+)!;
 
+circleBtns.forEach((item) =>
+	item.addEventListener('click', (e) => {
+		const crewItem: any = e.target as HTMLElement;
 
+		console.log(crewItem.value);
 
+		if (crewItem != null) {
+			switch (crewItem.value) {
+				case '0':
+					crewRole.innerText = jsonFile.crew[0].role;
+					memberName.innerText = jsonFile.crew[0].name;
+					description.innerText = jsonFile.crew[0].bio;
+					memberImg.src = './src/assets/crew/image-1.png';
+					break;
+				case '1':
+					crewRole.innerText = jsonFile.crew[1].role;
+					memberName.innerText = jsonFile.crew[1].name;
+					description.innerText = jsonFile.crew[1].bio;
+					memberImg.src = './src/assets/crew/image-2.png';
+					break;
+				case '2':
+					crewRole.innerText = jsonFile.crew[2].role;
+					memberName.innerText = jsonFile.crew[2].name;
+					description.innerText = jsonFile.crew[2].bio;
+					memberImg.src = './src/assets/crew/image-3.png';
+					break;
+				case '3':
+					crewRole.innerText = jsonFile.crew[3].role;
+					memberName.innerText = jsonFile.crew[3].name;
+					description.innerText = jsonFile.crew[3].bio;
+					memberImg.src = './src/assets/crew/image-4.png';
+					break;
 
-circleBtns.forEach((item) => item.addEventListener('click', (e) => {
-    const crewItem: any = e.target as HTMLElement;
+				default:
+					break;
+			}
+		} else {
+			console.error('crewItem = null');
+		}
+	})
+);
 
-    console.log(typeof crewItem.value);
-    
-    if(crewItem != null) {
-        
-        switch (crewItem.value) {
-            case "0":
-                crewRole.innerText = jsonFile.crew[0].role
-                memberName.innerText = jsonFile.crew[0].name
-                description.innerText = jsonFile.crew[0].bio
-                memberImg.src = "./src/assets/crew/image-1.png"
-                break;
-            case "1":
-                crewRole.innerText = jsonFile.crew[1].role
-                memberName.innerText = jsonFile.crew[1].name
-                description.innerText = jsonFile.crew[1].bio
-                memberImg.src = "./src/assets/crew/image-2.png"
-                break;
-            case "2":
-                crewRole.innerText = jsonFile.crew[2].role
-                memberName.innerText = jsonFile.crew[2].name
-                description.innerText = jsonFile.crew[2].bio
-                memberImg.src = "./src/assets/crew/image-3.png"
-                break;
-            case "3":
-                crewRole.innerText = jsonFile.crew[3].role
-                memberName.innerText = jsonFile.crew[3].name
-                description.innerText = jsonFile.crew[3].bio
-                memberImg.src = "./src/assets/crew/image-4.png"
-                break;
-        
-            default:
-                break;
-        }
-    }  else {
-        console.error('crewItem = null');
-        
-    }
-}))
+// 20.02.2024 updated code to toggle active class on buttons
+function activeBtn(this: HTMLButtonElement) {
+	closeActiveBtn();
+	this.classList.toggle('active');
+}
+
+function closeActiveBtn() {
+	circleBtns.forEach(btn => btn.classList.remove('active'))
+}
+
+circleBtns.forEach((item) => item.addEventListener('click', activeBtn));
